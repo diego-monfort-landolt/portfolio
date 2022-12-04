e3d9e66397f4a39f820c5ebd96fe7f642ad61bd8
Diego19890102 <diegomonfort1989@gmail.com>
2022-12-04 21:06:06 +0100

    jj
    j

--- main.js
+++ main.js
@@ -4,3 +4,9 @@ var typed = new Typed(".auto-type", {
     backSpeed:150,
     loop:true
 });
+
+const btnSwitch =document.getElementById('switch');
+btnSwitch.addEventListener('click', ()=>{
+    document.body.classList.toggle('dark');
+    btnSwitch.classList.toggle('active');
+});
